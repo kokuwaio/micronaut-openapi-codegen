4b5a937b64c67a47d01a49e2871a579f12d81c3e
Stefan Wiedemann <wistefan@googlemail.com>
2022-09-29 11:46:18 +0200

    update gen

--- gen/main/java/testmodel/examples/SecondLevelModel.java
+++
@@ -1,8 +0,0 @@
-package testmodel.examples;
-
-@jakarta.annotation.Generated("org.openapitools.codegen.languages.MicronautCodegen")
-@com.fasterxml.jackson.annotation.JsonTypeInfo(use = com.fasterxml.jackson.annotation.JsonTypeInfo.Id.DEDUCTION, visible = true)
-@com.fasterxml.jackson.annotation.JsonSubTypes({
-	@com.fasterxml.jackson.annotation.JsonSubTypes.Type(value = SecondLevelOneModel.class),
-	@com.fasterxml.jackson.annotation.JsonSubTypes.Type(value = SecondLevelTwoModel.class)})
-public interface SecondLevelModel extends OneOfModel {}
