6fb8163e41ad189acee49b46d8c81dcabd22c7c5
Stephan Schnabel <stephan.schnabel@posteo.de>
2023-09-07 13:33:56 +0200

    Testcase for issue #303

---
+++ gen/main/java/issue/_303/EmbeddedValue.java
@@ -0,0 +1,57 @@
+package issue._303;
+
+@jakarta.annotation.Generated("org.openapitools.codegen.languages.MicronautCodegen")
+@io.micronaut.core.annotation.Introspected
+public class EmbeddedValue {
+
+	public static final java.lang.String JSON_PROPERTY_TYPE = "type";
+
+	@com.fasterxml.jackson.annotation.JsonProperty(JSON_PROPERTY_TYPE)
+	@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
+	private java.lang.String type;
+
+	// methods
+
+	@Override
+	public boolean equals(Object object) {
+		if (object == this) {
+			return true;
+		}
+		if (object == null || getClass() != object.getClass()) {
+			return false;
+		}
+		EmbeddedValue other = (EmbeddedValue) object;
+		return java.util.Objects.equals(type, other.type);
+	}
+
+	@Override
+	public int hashCode() {
+		return java.util.Objects.hash(type);
+	}
+
+	@Override
+	public java.lang.String toString() {
+		return new java.lang.StringBuilder()
+				.append("EmbeddedValue[")
+				.append("type=").append(type)
+				.append("]")
+				.toString();
+	}
+
+	// fluent
+
+	public EmbeddedValue type(java.lang.String newType) {
+		this.type = newType;
+		return this;
+	}
+
+	// getter/setter
+
+	public java.lang.String getType() {
+		return type;
+	}
+
+	public void setType(java.lang.String newType) {
+		this.type = newType;
+	}
+}
