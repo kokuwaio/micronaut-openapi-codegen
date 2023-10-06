683763bf3e95f09c4159f08f68897d4e872b2418
Stephan Schnabel <stephan.schnabel@posteo.de>
2023-10-06 08:39:01 +0200

    Add invalid spec for #315

---
+++ gen/main/java/issue/_315/Credential.java
@@ -0,0 +1,57 @@
+package issue._315;
+
+@jakarta.annotation.Generated("org.openapitools.codegen.languages.MicronautCodegen")
+@io.micronaut.core.annotation.Introspected
+public class Credential {
+
+	public static final java.lang.String JSON_PROPERTY_DUMMY = "dummy";
+
+	@com.fasterxml.jackson.annotation.JsonProperty(JSON_PROPERTY_DUMMY)
+	@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
+	private java.lang.String dummy;
+
+	// methods
+
+	@Override
+	public boolean equals(Object object) {
+		if (object == this) {
+			return true;
+		}
+		if (object == null || getClass() != object.getClass()) {
+			return false;
+		}
+		Credential other = (Credential) object;
+		return java.util.Objects.equals(dummy, other.dummy);
+	}
+
+	@Override
+	public int hashCode() {
+		return java.util.Objects.hash(dummy);
+	}
+
+	@Override
+	public java.lang.String toString() {
+		return new java.lang.StringBuilder()
+				.append("Credential[")
+				.append("dummy=").append(dummy)
+				.append("]")
+				.toString();
+	}
+
+	// fluent
+
+	public Credential dummy(java.lang.String newDummy) {
+		this.dummy = newDummy;
+		return this;
+	}
+
+	// getter/setter
+
+	public java.lang.String getDummy() {
+		return dummy;
+	}
+
+	public void setDummy(java.lang.String newDummy) {
+		this.dummy = newDummy;
+	}
+}
---
+++ gen/main/java/issue/_315/ServiceScopesEntry.java
@@ -0,0 +1,34 @@
+package issue._315;
+
+@jakarta.annotation.Generated("org.openapitools.codegen.languages.MicronautCodegen")
+@io.micronaut.core.annotation.Introspected
+public class ServiceScopesEntry extends java.util.ArrayList<Credential> {
+
+
+	// methods
+
+	@Override
+	public boolean equals(Object object) {
+		if (object == this) {
+			return true;
+		}
+		if (object == null || getClass() != object.getClass()) {
+			return false;
+		}
+		return super.equals(object);
+	}
+
+	@Override
+	public int hashCode() {
+		return java.util.Objects.hash();
+	}
+
+	@Override
+	public java.lang.String toString() {
+		return new java.lang.StringBuilder()
+				.append("ServiceScopesEntry[")
+				.append("super").append(super.toString())
+				.append("]")
+				.toString();
+	}
+}
