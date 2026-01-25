a5daa54128abb302b3337c7e74972e18c4643b68
Stephan Schnabel <stephan.schnabel@grayc.de>
2026-01-25 12:35:52 +0100

    fix: changes by v7.19.0

--- gen/main/java/testmodel/spring/InheritanceWithoutType2.java
+++ gen/main/java/testmodel/spring/InheritanceWithoutType2.java
@@ -81,7 +81,7 @@ public class InheritanceWithoutType2 extends InheritanceWithoutType {
    * Convert the given object to string with each line indented by 4 spaces
    * (except the first line).
    */
-  private String toIndentedString(Object o) {
+  private String toIndentedString(@Nullable Object o) {
     if (o == null) {
       return "null";
     }
